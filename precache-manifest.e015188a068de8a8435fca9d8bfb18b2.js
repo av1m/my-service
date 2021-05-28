d899cda36c0bf4906c269f3b4c4e3f016e005bc5
av1m <36456709+av1m@users.noreply.github.com>
2021-05-28 15:32:51 +0000

    Deploying to gh-pages from @ av1m/my-service@be44afae3fd94e4bbd50cc31557672844152b891 🚀

---
+++ precache-manifest.e015188a068de8a8435fca9d8bfb18b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493fff0442c08dcd4373",
+    "url": "/my-service/css/app.8c9197fa.css"
+  },
+  {
+    "revision": "6589d6cc36d2f65e6a80",
+    "url": "/my-service/css/chunk-vendors.eb8b31b1.css"
+  },
+  {
+    "revision": "211da65f62ef817958454120cad5211f",
+    "url": "/my-service/img/icon.211da65f.svg"
+  },
+  {
+    "revision": "7fea6bdbf94b82ab5a9c7e1e37c1093d",
+    "url": "/my-service/img/logo.7fea6bdb.svg"
+  },
+  {
+    "revision": "9588aeb23373621be1a9b8a9c15e658f",
+    "url": "/my-service/index.html"
+  },
+  {
+    "revision": "493fff0442c08dcd4373",
+    "url": "/my-service/js/app.c9e2cb28.js"
+  },
+  {
+    "revision": "6589d6cc36d2f65e6a80",
+    "url": "/my-service/js/chunk-vendors.1d1b77a2.js"
+  },
+  {
+    "revision": "158ecbebf0adb4c670ed",
+    "url": "/my-service/js/signup.83017795.js"
+  },
+  {
+    "revision": "d174bcf3a711f5326135795a96f3b608",
+    "url": "/my-service/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-service/robots.txt"
+  }
+]);
